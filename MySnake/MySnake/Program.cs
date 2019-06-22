78be930612c7dbedbd8e4ccd650d9faf1e39157d
Your Name <veralazu@gmail.com>
2019-06-22 19:19:50 +0300

    Created new function Drow.

--- MySnake/MySnake/Program.cs
+++ MySnake/MySnake/Program.cs
@@ -6,7 +6,27 @@ namespace MySnake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sym1);
+
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+            Console.SetCursorPosition(x2, y2);
+            Console.Write(sym2);
+            Console.ReadLine();
+
+
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym );
         }
     }
 }
